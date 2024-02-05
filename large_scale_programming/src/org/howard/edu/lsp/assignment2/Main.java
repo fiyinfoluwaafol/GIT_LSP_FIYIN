08aeebe5c3b524d6e2550372fbc7cdb58b5affc6
Fiyinfoluwa Afolayan <fiyinfoluwa.afolayan@gmail.com>
2024-02-05 10:26:36 -0500

    Assignment #2 | Added comments to Main.java

--- large_scale_programming/src/org/howard/edu/lsp/assignment2/Main.java
+++ large_scale_programming/src/org/howard/edu/lsp/assignment2/Main.java
@@ -13,11 +13,14 @@ public class Main {
         String relativePath = "words.txt";
 
         try {
-        	//Print out the input file
+        	// Prints out the input file as a string to the console
             String fileContent = FileReader.readFileAsString(relativePath);
             System.out.println("The input file is as follows: ");
             System.out.println(fileContent);
-            List<String> tokenList = FileReader.tokenizeString(fileContent);
+            
+            List<String> tokenList = FileReader.tokenizeString(fileContent); // Tokenizes the input file string
+            
+            // Iterates through string tokens and keeps track of each valid (non-numeric or non-trivial) token's occurrence in a HashMap
             System.out.println("Here is the word count for the input file");
             for (String token : tokenList) {
             	if (FileReader.isNumeric(token) || token.length()<=3 ) {
@@ -30,6 +33,7 @@ public class Main {
             	}
             }
             
+            // Prints out word count for each word in HashMap
             for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
                 System.out.println(entry.getKey() + ": " + entry.getValue());
             }
